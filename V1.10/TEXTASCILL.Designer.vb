bc95cc8c6ae401aa096b0266983bc0209c89a6c0
CQSSA <oclockoilp@outlook.com>
2025-05-20 18:46:40 +0800

    Add files via upload
    Major improvements, adding a transparent gradient for window closing, made with VB2010 (NET4.0)
    使用VB2010重新制作

---
+++ V1.10/TEXTASCILL.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WINDOW
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TTOA = New System.Windows.Forms.Button()
+        Me.ATOTE = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TIN = New System.Windows.Forms.TextBox()
+        Me.ASCI = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TTOA
+        '
+        Me.TTOA.Location = New System.Drawing.Point(80, 232)
+        Me.TTOA.Name = "TTOA"
+        Me.TTOA.Size = New System.Drawing.Size(212, 47)
+        Me.TTOA.TabIndex = 0
+        Me.TTOA.Text = "TEXT TO ASCILL"
+        Me.TTOA.UseVisualStyleBackColor = True
+        '
+        'ATOTE
+        '
+        Me.ATOTE.Location = New System.Drawing.Point(80, 291)
+        Me.ATOTE.Name = "ATOTE"
+        Me.ATOTE.Size = New System.Drawing.Size(212, 47)
+        Me.ATOTE.TabIndex = 1
+        Me.ATOTE.Text = "ASCILL TO TEXT"
+        Me.ATOTE.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微软雅黑", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(55, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 27)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "TEXT"
+        '
+        'TIN
+        '
+        Me.TIN.Location = New System.Drawing.Point(60, 67)
+        Me.TIN.Multiline = True
+        Me.TIN.Name = "TIN"
+        Me.TIN.Size = New System.Drawing.Size(250, 53)
+        Me.TIN.TabIndex = 3
+        '
+        'ASCI
+        '
+        Me.ASCI.Location = New System.Drawing.Point(60, 156)
+        Me.ASCI.Multiline = True
+        Me.ASCI.Name = "ASCI"
+        Me.ASCI.Size = New System.Drawing.Size(250, 53)
+        Me.ASCI.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微软雅黑", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(55, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 27)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ASCILL"
+        '
+        'WINDOW
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 350)
+        Me.Controls.Add(Me.ASCI)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TIN)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ATOTE)
+        Me.Controls.Add(Me.TTOA)
+        Me.MaximizeBox = False
+        Me.Name = "WINDOW"
+        Me.Text = "TEXTASCILL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TTOA As System.Windows.Forms.Button
+    Friend WithEvents ATOTE As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TIN As System.Windows.Forms.TextBox
+    Friend WithEvents ASCI As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ V1.10/TEXTASCILL.vb
@@ -0,0 +1,115 @@
+﻿Option Explicit On
+Public Class WINDOW
+    Dim M2, M3, ASCNUM, FOR1 As Integer
+    Dim T1, T2, TEXTIN, ASCIN As String
+    Dim ASC1(99999999) As String
+    Dim closeaira As Integer
+
+    Declare Function ReleaseCapture Lib "user32" () As Long
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hwnd As Long, ByVal wMsg As Long, ByVal wParam As Long, ByVal lParam As IntPtr) As Long
+    Private Declare Function SetLayeredWindowAttributes Lib "user32" (ByVal hwnd As Long, ByVal crKey As Long, ByVal bAlpha As Byte, ByVal dwFlags As Long) As Long
+    Private Declare Function GetWindowLong Lib "user32" Alias "GetWindowLongA" (ByVal hwnd As Long, ByVal nIndex As Long) As Long
+    Private Declare Function SetWindowLong Lib "user32" Alias "SetWindowLongA" (ByVal hwnd As Long, ByVal nIndex As Long, ByVal dwNewLong As Long) As Long
+    Private Declare Sub Sleep Lib "kernel32" (ByVal dwMilliseconds As Long)
+    '窗口透明常数 AIR NUMMBER
+    Const WS_EX_LAYERED = &H80000
+    Const GWL_EXSTYLE = (-20)
+    Const LWA_ALPHA = &H2
+    Const LWA_COLORKEY = &H1
+    '=====
+    Private Declare Function SetWindowPos Lib "user32" (ByVal hwnd As Long, ByVal hWndInsertAfter As Long, ByVal x As Long, ByVal y As Long, ByVal cx As Long, ByVal cy As Long, ByVal wFlags As Long) As Long
+    Private Const HWND_TOPMOST& = -1
+    ' 将窗口置于列表顶部，并位于任何最顶部窗口的前面
+    Private Const SWP_NOSIZE& = &H1
+    ' 保持窗口大小
+    Private Const SWP_NOMOVE& = &H2
+    ' 保持窗口位置
+    '窗口透明常数
+    '=====
+    Dim NUM1 As Double
+
+    Private Sub WINDOW_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim rtn As Long
+        Me.BackColor = System.Drawing.Color.FromArgb(158, 158, 158)  '设置一下窗口的颜色 SET WINDOW COLOR
+        rtn = GetWindowLong(Me.Handle, GWL_EXSTYLE)
+        rtn = rtn Or WS_EX_LAYERED
+        SetWindowLong(Me.Handle, GWL_EXSTYLE, rtn)
+        'SetWindowPos(Me.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE Or SWP_NOSIZE)
+        closeaira = 250
+        SetLayeredWindowAttributes(Me.Handle, RGB(0, 0, 0), 250, LWA_ALPHA)  'WINDOW AIR
+
+        
+
+    End Sub
+    Private Sub WINDOW_Closed(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Closed  'vb关闭事件 CLOSE WINDOW
+        For numas As Integer = 0 To 150
+            If closeaira > 1 Then
+                SetLayeredWindowAttributes(Me.Handle, RGB(0, 0, 0), closeaira, LWA_ALPHA)
+                closeaira = closeaira - 5
+            End If
+            Sleep(1)
+        Next
+    End Sub
+    Private Sub TTOA_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TTOA.Click
+        Erase ASC1
+        ReDim ASC1(99999999)
+        M2 = 0
+        M3 = 0
+        ASCNUM = 0
+        FOR1 = 0
+        T2 = ("")
+
+        TEXTIN = TIN.Text
+        T2 = ("/")
+
+        For Me.M2 = 0 To Len(TEXTIN)
+            If M2 >= 1 Then
+                M3 = M3 + 1
+                T1 = Mid(TEXTIN, M2, 1)
+                T2 = T2 + Format(Asc(T1)) + ("/")
+            End If
+        Next
+
+        ASCI.Text = T2
+
+    End Sub
+
+    Private Sub ATOTE_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ATOTE.Click
+        Erase ASC1
+        ReDim ASC1(99999999)
+        M2 = 0
+        M3 = 0
+        ASCNUM = 0
+        FOR1 = 0
+
+        T2 = ("")
+
+        ASCIN = ASCI.Text
+
+        For Me.M2 = 0 To Len(ASCIN)
+            If M2 >= 1 Then
+                If Mid(ASCIN, M2, 1) = ("/") Then
+                    ASCNUM = ASCNUM + 1
+                Else
+                    ASC1(ASCNUM) = ASC1(ASCNUM) + Mid(ASCIN, M2, 1)
+                End If
+            End If
+        Next
+
+        FOR1 = 1
+
+        For Me.M2 = 0 To (ASCNUM - 2)
+            T2 = T2 + Chr(ASC1(FOR1))
+            FOR1 = FOR1 + 1
+        Next
+        TIN.Text = T2
+    End Sub
+
+    Private Sub TIN_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TIN.TextChanged
+
+    End Sub
+
+    Private Sub ASC_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ASCI.TextChanged
+
+    End Sub
+End Class
